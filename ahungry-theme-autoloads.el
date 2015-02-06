7700da195a42c3aea92fa913e244ead94abf8c8b
Matthew Carter <m@ahungry.com>
2015-02-06 09:44:14 -0500

    Add proper auto load comment/code in the theme file

--- ahungry-theme-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; ahungry-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-(when (boundp 'custom-theme-load-path)
-  (add-to-list 'custom-theme-load-path (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ahungry-theme-pkg.el" "ahungry-theme.el"
-;;;;;;  "color-theme-ahungry.el") (21716 20244 379937 516000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ahungry-theme-autoloads.el ends here
--- ahungry-theme.el
+++ ahungry-theme.el
@@ -186,6 +186,12 @@
    '(red "#ffffff"))
   )
 
+;;;###autoload
+(when (and load-file-name (boundp 'custom-theme-load-path))
+ (add-to-list
+      'custom-theme-load-path
+      (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'ahungry)
 
 ;;; ahungry-theme.el ends here

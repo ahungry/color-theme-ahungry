935758ecac4c5906be47495b4fb3ac47e8ff5ac4
Matthew Carter <m@ahungry.com>
2019-12-13 16:14:49 -0500

    Stick to familiar constant name face

--- ahungry-theme.el
+++ ahungry-theme.el
@@ -275,7 +275,7 @@ Default value:
    '(web-mode-html-tag-bracket-face ((t (:foreground "#555"))))
    '(web-mode-html-tag-face ((t (:foreground "#ccc"))))
    '(web-mode-variable-name-face ((t (:foreground "#af0"))))
-   '(web-mode-constant-face ((t (:foreground "#fa0"))))
+   '(web-mode-constant-face ((t (:foreground "#fff900"))))
    '(web-mode-function-call-face ((t (:foreground "#f06"))))
    '(tide-hl-identifier-face ((t (:foreground "#f0a" :bold t))))
    '(helm-ff-directory ((t (:background "gold1" :foreground "#000000" :bold t))))
